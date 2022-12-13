729cef2e5a206dcb79c68c628c68471a80ebda8e
sheip9 <53911290+sheip9@users.noreply.github.com>
2022-12-13 17:55:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+git clone https://github.com/sheip9/luci-app-quickstart-lite  package/lean/luci-app-quickstart-lite
