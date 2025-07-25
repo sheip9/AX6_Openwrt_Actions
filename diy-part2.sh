726ee72a1c8b0b845d70c5f3ccf2c1b40aed643e
sheip9 <53911290+sheip9@users.noreply.github.com>
2025-07-25 08:58:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,7 @@
 
 # Modify default IP
 sed -i 's/LEDE/RedmiAX6/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/RedmiAX6/g' package/base-files/luci2/bin/config_generate
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/#5e72e4/#E7651A/g' package/feeds/kenzo/luci-app-argon-config/root/etc/config/argon
 rm ./feeds/small8/luci-app-pppoe-relay/root/etc/init.d/pppoe-relay -f
